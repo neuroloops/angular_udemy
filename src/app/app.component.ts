8b3f73ce2e5e95f200bd643aa607e1c0155e4eb0
Ludo Loops <ludoloops@prontonmail.com>
2020-07-21 01:08:41 +0200

    feat: 25 listening to your Custom Event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-essentials';
+  rootName = 'Ludo';
+
+  onNameChanged(newName) {
+    this.rootName = newName;
+  }
 }
