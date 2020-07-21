ac06e1cc3b10e48e8ba6ed5c8edf58c669a2633b
Ludo Loops <ludoloops@prontonmail.com>
2020-07-21 17:57:43 +0200

    feat: 40 what are directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-essentials';
-  rootName = 'Ludo';
+  rootItems = ['Apples', 'Bananas', 'Cherries'];
 
-  onNameChanged(newName) {
-    this.rootName = newName;
+  onItemWasAdded(newItem) {
+    this.rootItems.push(newItem);
+    console.log(this.rootItems);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent, UserDetailComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
